5e43361d695de15ac36928f070eaffc8ca99c2ac
jLereback <112405931+jLereback@users.noreply.github.com>
2023-03-05 22:03:05 +0100

    Update CountryResource.java

--- src/main/java/se/iths/enterprise/lab1/country/CountryResource.java
+++ src/main/java/se/iths/enterprise/lab1/country/CountryResource.java
@@ -1,8 +1,16 @@
 package se.iths.enterprise.lab1.country;
 
 import jakarta.inject.Inject;
+import jakarta.validation.Valid;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import se.iths.enterprise.lab1.country.dto.CountryDto;
+import se.iths.enterprise.lab1.country.dto.Mapper;
+import se.iths.enterprise.lab1.exceptions.IdNotFoundException;
+
+import java.net.URI;
+import java.util.List;
 
 @Path("/countries")
 public class CountryResource {
@@ -10,30 +18,46 @@ public class CountryResource {
     @Inject
     CountryRepository repository;
 
+    @Inject
+    Mapper mapper;
+
     @GET
-    @Path("/country/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Country getCountry(@PathParam("id") Long id) {
-        return repository.getCountry(id);
+    public List<CountryDto> getAllCountries() {
+        return mapper.map(repository.findAll());
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public CountryDto getCountry(@PathParam("id") Long id) {
+        if (repository.findOne(id).isEmpty())
+            throw new IdNotFoundException("Id_not_found: " + id);
+        return mapper.map(repository.getCountry(id));
     }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    public void addCountry(Country country) {
+    public Response addCountry(@Valid Country country) {
         repository.addCountry(country);
+        return Response.created(URI.create("countries/" + country.getId())).build();
     }
 
     @PUT
-    @Path("/update/{id}-{change}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public void updateCountry(@PathParam("id") Long id, @PathParam("change") String change) {
+    @Path("/{id}-{change}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response updateCountry(@PathParam("id") Long id, @PathParam("change") String change) {
+        var country = repository.findOne(id);
+        if (country.isEmpty())
+            throw new IdNotFoundException("Id_not_found: " + id);
         repository.updateCountry(id, change);
+        return Response.ok(repository.findOne(id)).build();
     }
 
     @DELETE
-    @Path("/remove/{id}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public void removeCountry(@PathParam("id") Long id) {
+    @Path("/{id}")
+    public Response removeCountry(@PathParam("id") Long id) {
         repository.removeCountry(id);
+        return Response.ok().build();
     }
 }
